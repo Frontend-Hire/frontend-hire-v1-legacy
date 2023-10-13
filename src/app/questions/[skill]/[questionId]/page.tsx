70c5eff9d3afa0aa47b742e8430d11e7e8bf3dfa
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-10-14 00:10:12 +0530

    Config

--- src/app/questions/[skill]/[questionId]/page.tsx
+++ src/app/questions/[skill]/[questionId]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Layout from '@/components/Layout';
+import { Skill, getQuestionConfig } from '@/utils/template';
 import {
   SandpackProvider,
   SandpackCodeEditor,
@@ -10,35 +11,31 @@ import {
 export default function Question({
   params,
 }: {
-  params: { questionId: string };
+  params: { questionId: string; skill: string };
 }) {
-  const { questionId } = params;
+  const { questionId, skill } = params;
+
+  const { template } = getQuestionConfig(skill.toLowerCase() as Skill);
 
   return (
-    <section className="h-full">
-      <SandpackProvider
-        style={{
-          height: '100%',
-        }}
-        template="static"
-        options={{
-          autoReload: true,
-          autorun: false,
-        }}
-      >
-        <Layout
-          topLeft={<div className="h-full">Question</div>}
-          topRight={
-            <SandpackCodeEditor
-              showLineNumbers
-              wrapContent
-              className="h-full"
-            />
-          }
-          bottomLeft={<div className="h-full">Expected Output</div>}
-          bottomRight={<SandpackPreview className="h-full" />}
-        />
-      </SandpackProvider>
-    </section>
+    <SandpackProvider
+      style={{
+        height: '100%',
+      }}
+      template={template}
+      options={{
+        autoReload: true,
+        autorun: false,
+      }}
+    >
+      <Layout
+        topLeft={<div className="h-full">Question</div>}
+        topRight={
+          <SandpackCodeEditor showLineNumbers wrapContent className="h-full" />
+        }
+        bottomLeft={<div className="h-full">Expected Output</div>}
+        bottomRight={<SandpackPreview className="h-full" />}
+      />
+    </SandpackProvider>
   );
 }
