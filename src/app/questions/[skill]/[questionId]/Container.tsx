a1ea0d5f3fd9d0f4177dcf6e59a54d59f40bc9be
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-10-15 20:51:43 +0530

    Tabs

--- src/app/questions/[skill]/[questionId]/Container.tsx
+++ src/app/questions/[skill]/[questionId]/Container.tsx
@@ -2,6 +2,7 @@ import QuestionLayout from '@/components/QuestionLayout';
 import QuestionLayoutItem from '@/components/QuestionLayoutItem';
 import {
   SandpackCodeEditor,
+  SandpackConsole,
   SandpackPreview,
   useSandpack,
 } from '@codesandbox/sandpack-react';
@@ -10,7 +11,7 @@ import Tooltip from '@/components/Tooltip';
 
 import { IQuestionSuccess } from './useQuestion';
 import Button from '@/components/Button';
-import { useEffect } from 'react';
+import { ITab } from '@/components/Tabs';
 
 interface Props {
   data: IQuestionSuccess;
@@ -19,72 +20,87 @@ interface Props {
 export default function Container({ data }: Props) {
   const { sandpack } = useSandpack();
 
+  const topLeft = () => (
+    <QuestionLayoutItem
+      tabs={[
+        {
+          label: 'Question',
+          value: 'Question',
+          content: <div className="prose">{data.question.getContent()}</div>,
+        },
+      ]}
+    />
+  );
+
+  const topRight = () => (
+    <QuestionLayoutItem
+      rightButtons={
+        <Button className="rounded-none" onClick={sandpack.runSandpack}>
+          Run Code
+        </Button>
+      }
+      tabs={[
+        {
+          label: 'Editor',
+          value: 'Editor',
+          content: (
+            <SandpackCodeEditor
+              showTabs
+              showLineNumbers
+              showRunButton={false}
+              wrapContent
+              className="h-full"
+            />
+          ),
+        },
+      ]}
+    />
+  );
+
+  const bottomLeft = () =>
+    data.question.meta.expectedOutput ? (
+      <div className="h-full">Expected Output</div>
+    ) : (
+      ''
+    );
+
+  const bottomRight = () => {
+    const tabs: ITab[] = [];
+
+    if (data.question.meta.showPreview) {
+      tabs.push({
+        label: 'Preview',
+        value: 'Preview',
+        content: <SandpackPreview showNavigator className="h-full" />,
+      });
+    }
+
+    if (data.question.meta.showConsole) {
+      tabs.push({
+        label: 'Console',
+        value: 'Console',
+        content: <SandpackConsole className="h-full" />,
+      });
+    }
+
+    return (
+      <QuestionLayoutItem
+        rightButtons={
+          <Tooltip title="Click on Run Code in the Code Editor to see preview">
+            <InfoIcon className="mx-2 cursor-pointer hover:opacity-70" />
+          </Tooltip>
+        }
+        tabs={tabs}
+      />
+    );
+  };
+
   return (
     <QuestionLayout
-      topLeft={
-        <QuestionLayoutItem
-          tabs={[
-            {
-              label: 'Question',
-              value: 'Question',
-              content: (
-                <div className="prose">{data.question.getContent()}</div>
-              ),
-            },
-          ]}
-        />
-      }
-      topRight={
-        <QuestionLayoutItem
-          rightButtons={
-            <Button className="rounded-none" onClick={sandpack.runSandpack}>
-              Run Code
-            </Button>
-          }
-          tabs={[
-            {
-              label: 'Editor',
-              value: 'Editor',
-              content: (
-                <SandpackCodeEditor
-                  showTabs
-                  showLineNumbers
-                  showRunButton={false}
-                  wrapContent
-                  className="h-full"
-                />
-              ),
-            },
-          ]}
-        />
-      }
-      bottomLeft={
-        data.question.meta.expectedOutput ? (
-          <div className="h-full">Expected Output</div>
-        ) : (
-          ''
-        )
-      }
-      bottomRight={
-        data.question.meta.showPreview ? (
-          <QuestionLayoutItem
-            rightButtons={
-              <Tooltip title="Click on Run Code in the Code Editor to see preview">
-                <InfoIcon className="mx-2 cursor-pointer hover:opacity-70" />
-              </Tooltip>
-            }
-            tabs={[
-              {
-                label: 'Preview',
-                value: 'Preview',
-                content: <SandpackPreview className="h-full" />,
-              },
-            ]}
-          />
-        ) : (
-          ''
-        )
-      }
+      topLeft={topLeft()}
+      topRight={topRight()}
+      bottomLeft={bottomLeft()}
+      bottomRight={bottomRight()}
     />
   );
 }
