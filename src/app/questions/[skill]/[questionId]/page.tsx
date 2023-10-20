ec12fbd10559c058746bfd38747ca937eedff5d9
Hruthik Reddy Yarala <32637966+yaralahruthik@users.noreply.github.com>
2023-10-20 22:24:31 +0530

    Hotkeys for select all and save (#2)

--- src/app/questions/[skill]/[questionId]/page.tsx
+++ src/app/questions/[skill]/[questionId]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { SandpackProvider } from '@codesandbox/sandpack-react';
 import * as React from 'react';
+
+import { SandpackProvider } from '@codesandbox/sandpack-react';
 import QuestionLayoutSkeleton from '@/components/QuestionLayoutSkeleton';
 import useQuestion from './_hooks/useQuestion';
 import Container from './_components/Container';
+import QuestionHotkeysProvider from './_components/QuestionHotkeysProvider';
 
 export default function Question({
   params,
@@ -20,19 +22,21 @@ export default function Question({
   if (data.status === 'error') return '---------------ERROR---------------';
 
   return (
-    <SandpackProvider
-      style={{
-        height: '100%',
-      }}
-      template={data.question.meta.template}
-      files={data.question.meta.files}
-      options={{
-        externalResources: data.question.meta.externalCDNs,
-        autoReload: true,
-        autorun: false, // If true results in infinite loader
-      }}
-    >
-      <Container data={data} />
-    </SandpackProvider>
+    <QuestionHotkeysProvider>
+      <SandpackProvider
+        style={{
+          height: '100%',
+        }}
+        template={data.question.meta.template}
+        files={data.question.meta.files}
+        options={{
+          externalResources: data.question.meta.externalCDNs,
+          autoReload: true,
+          autorun: false, // If true results in infinite loader
+        }}
+      >
+        <Container data={data} />
+      </SandpackProvider>
+    </QuestionHotkeysProvider>
   );
 }
