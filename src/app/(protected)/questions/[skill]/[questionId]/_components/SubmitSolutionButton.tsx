ed19ede48835900179b06ef6d7656cbe1380f69e
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-10-24 15:40:29 +0530

    Submit Button

---
+++ src/app/(protected)/questions/[skill]/[questionId]/_components/SubmitSolutionButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function SubmitSolutionButton() {
+  return (
+    <Button className="rounded-t-none bg-green-600 hover:bg-green-700">
+      Submit Solution
+    </Button>
+  );
+}
