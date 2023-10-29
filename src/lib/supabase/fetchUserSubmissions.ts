92e2e72a38da92f0355598d4a261e8ec6f088547
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-10-29 20:51:14 +0530

    feat: question status

--- src/lib/supabase/fetchUserSubmissions.ts
+++ src/lib/supabase/fetchUserSubmissions.ts
@@ -2,9 +2,11 @@ import { Database } from '@/types/supabase';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
-const supabaseServerClient = createServerComponentClient<Database>({ cookies });
-
 export default async function fetchUserSubmissions() {
+  const supabaseServerClient = createServerComponentClient<Database>({
+    cookies,
+  });
+
   const { data } = await supabaseServerClient
     .from('code_submissions')
     .select('question_id');
