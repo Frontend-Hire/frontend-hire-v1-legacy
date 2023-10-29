2dc9cbb867ff9db7289b561bd26961ce308c2cf8
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-10-29 18:46:36 +0530

    feat: fetch submitted questions

--- src/app/(protected)/dashboard/_components/Overview/Overview.tsx
+++ src/app/(protected)/dashboard/_components/Overview/Overview.tsx
@@ -1,12 +1,19 @@
 import { getTotalQuestionsFromLocal } from '@/lib/fetchLocalFiles';
 import SubmittedQuestions from './SubmittedQuestions';
+import fetchUserSubmissions from '@/lib/supabase/fetchUserSubmissions';
 
 export default async function Overview() {
-  const totalQuestions = await getTotalQuestionsFromLocal();
+  const [totalQuestions, submittedQuestions] = await Promise.all([
+    getTotalQuestionsFromLocal(),
+    fetchUserSubmissions(),
+  ]);
 
   return (
     <div className="grid h-[300px] grid-cols-1 rounded-md bg-white p-2 sm:grid-cols-2 md:grid-cols-3">
-      <SubmittedQuestions submitted={3} total={totalQuestions} />
+      <SubmittedQuestions
+        submitted={submittedQuestions.length}
+        total={totalQuestions}
+      />
     </div>
   );
 }
