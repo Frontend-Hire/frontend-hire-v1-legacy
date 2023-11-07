fcf19c4aab5d50ff88ac756e43928be6fce974a8
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-11-07 20:11:55 +0530

    feat: improved sign in flow

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import SignInButton from '@/components/SignInButton';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
@@ -19,8 +20,11 @@ export default async function ProtectedLayout({
     return (
       <>
         <Header />
-        <div className="flex flex-col items-center justify-center">
-          Sign In To Access This Page!
+        <div className="flex flex-col items-center justify-center gap-6 pt-4">
+          <SignInButton />
+          <p className="font-medium">
+            Support For More Sign In Providers Coming Soon!
+          </p>
         </div>
       </>
     );
