eb9bd585dad9a4122ab8e99ace3bac45ead02954
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-11-15 20:07:56 +0530

    feat: delete account

--- src/app/(protected)/settings/_components/DeleteAccountButtonWithAlert.tsx
+++ src/app/(protected)/settings/_components/DeleteAccountButtonWithAlert.tsx
@@ -1,20 +1,67 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 
 export default function DeleteAccountButtonWithAlert() {
   const supabaseBrowserClient = createClientComponentClient();
+  const router = useRouter();
 
-  const deleteAccount = async () => {
+  const handleDelete = async () => {
     try {
-      console.log('Deleted Account');
+      const {
+        data: { user },
+      } = await supabaseBrowserClient.auth.getUser();
+      if (user) {
+        await fetch('http://127.0.0.1:3000/settings/account', {
+          method: 'DELETE',
+          body: JSON.stringify({ id: user.id }),
+        });
+        await supabaseBrowserClient.auth.signOut();
+        router.replace('/');
+      }
     } catch (e) {
       console.log(e);
     }
   };
 
   return (
-    <Button variant="destructive" className="min-w-[200px]">
-      Delete Account
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="min-w-[200px]">
+          Delete Account
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Delete Account</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will delete your account and any data related to it. This is
+            irreversible. Are you sure?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-destructive hover:bg-destructive/90"
+            onClick={handleDelete}
+          >
+            Yes, Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
