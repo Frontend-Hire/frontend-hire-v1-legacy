0ee5fd97c62ad962fc6d219281711ceb1e53073f
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-11-20 18:51:04 +0530

    feat: mobile responsive

--- src/app/(protected)/questions/[skill]/[questionId]/_layout/PrimaryLayout.tsx
+++ src/app/(protected)/questions/[skill]/[questionId]/_layout/PrimaryLayout.tsx
@@ -7,7 +7,7 @@ export default function PrimaryLayout({ header, children }: Props) {
   return (
     <div className="flex h-full flex-col border-x-[10px] border-gray-400 bg-gray-400">
       {header}
-      <main className="h-0 flex-grow border-t-[10px] border-gray-400 bg-white">
+      <main className="flex grow flex-col border-t-[10px] border-gray-400 bg-white">
         {children}
       </main>
     </div>
---
+++ src/components/QuestionLayout/QuestionMobileLayout.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import * as React from 'react';
+import { Tabs, TabsList, TabsTrigger } from '../ui/tabs';
+import { TabsContent } from '@radix-ui/react-tabs';
+
+interface Tab {
+  label: string;
+  content: React.ReactNode;
+}
+
+interface Props {
+  tabs: (Tab | undefined)[];
+}
+
+export default function QuestionMobileLayout({ tabs }: Props) {
+  const tabsCleaned: NonNullable<Tab>[] = tabs.filter(
+    (tab) => tab !== undefined,
+  ) as NonNullable<Tab>[];
+
+  const [activeTab, setActiveTab] = React.useState(tabsCleaned[0].label);
+  return (
+    <Tabs
+      className="flex h-full flex-col"
+      value={activeTab}
+      onValueChange={setActiveTab}
+    >
+      <div className="flex h-[40px] items-center justify-between bg-gray-600">
+        <TabsList>
+          {tabsCleaned.map((tab) => (
+            <TabsTrigger key={tab.label} value={tab.label}>
+              {tab.label}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </div>
+      {tabsCleaned.map((tab) => (
+        <TabsContent
+          className="flex h-full flex-col"
+          asChild
+          forceMount
+          key={tab.label}
+          value={tab.label}
+        >
+          <div className="h-0 flex-grow overflow-auto data-[state=inactive]:hidden">
+            {tab.content}
+          </div>
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
