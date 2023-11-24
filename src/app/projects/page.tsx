eeca3b06be493a40a31d9f5bccfb4f1660980015
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-11-25 00:17:06 +0530

    feat: project item

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,29 @@
+import DifficultyLegend from '@/components/DifficultyLegend';
 import Heading from '@/components/Heading';
+import ProjectItem from '@/components/ProjectItem';
+import { getProjectsFromLocal } from '@/lib/fetchLocalFiles';
 
-export default async function Questions() {
+export default async function Projects() {
+  const data = await getProjectsFromLocal();
+
   return (
     <main>
       <Heading variant="h1" className="mb-8 text-center">
         Projects
       </Heading>
+      <div className="mb-4">
+        <DifficultyLegend />
+      </div>
 
       <div className="grid grid-cols-1 justify-center justify-items-stretch gap-4 px-4 md:grid-cols-2 lg:grid-cols-3">
-        Coming Soon!
+        {data.map((project) => (
+          <ProjectItem
+            key={project.id}
+            tasks={project.tasks}
+            title={project.title}
+            difficulty={project.difficulty}
+          />
+        ))}
       </div>
     </main>
   );
