83e18c1bfdd9d464acb80dd6389667a4c2eff2f7
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-11-30 00:32:31 +0530

    feat: color improvement

--- src/components/DifficultyBadge/DifficultyBadge.tsx
+++ src/components/DifficultyBadge/DifficultyBadge.tsx
@@ -5,10 +5,10 @@ import { cva } from 'class-variance-authority';
 const badge = cva('font-semibold rounded-full mx-2', {
   variants: {
     intent: {
-      [DIFFICULTY.EASY]: 'bg-green-300',
-      [DIFFICULTY.MEDIUM]: 'bg-yellow-300',
-      [DIFFICULTY.HARD]: 'bg-red-300',
-      [DIFFICULTY.MASTER]: 'bg-gray-300',
+      [DIFFICULTY.EASY]: 'bg-green-500',
+      [DIFFICULTY.MEDIUM]: 'bg-yellow-500',
+      [DIFFICULTY.HARD]: 'bg-red-500',
+      [DIFFICULTY.MASTER]: 'bg-gray-500',
     },
     size: {
       small: ['text-sm', 'py-1', 'px-2'],
