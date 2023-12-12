3083a617516f735753117c67c9eb8076659c0bf3
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-12-12 15:29:21 +0530

    fix: landing page responsiveness

--- src/app/(landing page)/page.tsx
+++ src/app/(landing page)/page.tsx
@@ -16,44 +16,94 @@ import HuskyLogo from '@/assets/toollogos/husky.png';
 export default function Home() {
   return (
     <main className="h-full">
-      <section className="relative flex h-full flex-col items-center justify-center gap-[25px] p-[10px] text-center md:px-[40px] md:py-[10px]">
-        <p className="rounded-[5px] bg-[#18005F] px-[10px] py-[5px] text-sm md:text-base">
-          100% Free Access to Questions and Projects
-        </p>
-        <h1 className="max-w-[25ch] text-4xl font-black md:text-5xl">
-          Practice Core Frontend Skills For Interviews And Real World
-        </h1>
-        <div className="flex items-center gap-[10px]">
-          <Line />
-          <h2 className="whitespace-nowrap text-xl font-bold md:text-2xl">
-            Make A Hiring Profile And Get Hired *
-          </h2>
-          <Line />
+      <section className="flex h-full flex-col items-center justify-center gap-[25px] p-[10px] text-center md:px-[40px] md:py-[10px]">
+        <div className="flex h-full grow flex-col items-center justify-center gap-[25px]">
+          <p className="rounded-[5px] bg-[#18005F] px-[10px] py-[5px] text-sm md:text-base">
+            100% Free Access to Questions and Projects
+          </p>
+          <h1 className="max-w-[25ch] text-4xl font-black md:text-5xl">
+            Practice Core Frontend Skills For Interviews And Real World
+          </h1>
+          <div className="flex items-center gap-[10px]">
+            <Line />
+            <h2 className="whitespace-nowrap text-xl font-bold md:text-2xl">
+              Make A Hiring Profile And Get Hired *
+            </h2>
+            <Line />
+          </div>
+          <SignInButton label="Just Need A Google Account To Get Started " />
         </div>
-        <SignInButton label="Just Need A Google Account To Get Started " />
 
-        <div className="absolute bottom-4 flex flex-col gap-[20px]">
+        <div className="flex flex-col gap-[20px]">
           <p className="max-w-fit self-center rounded-[5px] bg-destructive px-[5px] py-[2.5px] text-xs md:text-sm">
             * Currently in Beta! Hiring Profiles available early 2024!
           </p>
-          <p className="font-bold">
+          <p className="text-sm font-bold md:text-base">
             A Growing Library of{' '}
             <span className="text-primary">7+ Questions</span> and{' '}
             <span className="text-primary">2+ Projects</span>
           </p>
           <div className="flex flex-wrap items-center justify-center gap-[20px] md:gap-[25px]">
-            <Image src={HTMLLogo} alt="HTML Logo" />
-            <Image src={CSSLogo} alt="CSS Logo" />
-            <Image src={JSLogo} alt="JavaScript Logo" />
-            <Image src={TSLogo} alt="TypeScript Logo" />
-            <Image src={ReactLogo} alt="React Logo" />
-            <Image src={ViteLogo} alt="Vite Logo" />
-            <Image src={ParcelLogo} alt="Parcel Logo" />
-            <Image src={JestLogo} alt="Jest Logo" />
-            <Image src={PrettierLogo} alt="Prettier Logo" />
-            <Image src={StoryBookLogo} alt="StoryBook Logo" />
-            <Image src={ESLintLogo} alt="ESLint Logo" />
-            <Image src={HuskyLogo} alt="Husky Logo" />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={HTMLLogo}
+              alt="HTML Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={CSSLogo}
+              alt="CSS Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={JSLogo}
+              alt="JavaScript Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={TSLogo}
+              alt="TypeScript Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={ReactLogo}
+              alt="React Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={ViteLogo}
+              alt="Vite Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={ParcelLogo}
+              alt="Parcel Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={JestLogo}
+              alt="Jest Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={PrettierLogo}
+              alt="Prettier Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={StoryBookLogo}
+              alt="StoryBook Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={ESLintLogo}
+              alt="ESLint Logo"
+            />
+            <Image
+              className="h-[24px] w-max md:h-[40px]"
+              src={HuskyLogo}
+              alt="Husky Logo"
+            />
           </div>
         </div>
       </section>
