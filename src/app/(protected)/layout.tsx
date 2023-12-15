ccec683506f3e6baecd3d8bfbdefcfdf95d72a1f
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-12-15 18:23:32 +0530

    feat: protected layout message

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import Heading from '@/components/Heading';
 import SignInButton from '@/components/SignInButton';
 import createSupabaseServerClient from '@/lib/supabase/supabaseServerClient';
 
@@ -16,10 +17,21 @@ export default async function ProtectedLayout({
     return (
       <>
         <Header />
-        <div className="flex flex-col items-center justify-center gap-6 pt-4">
+        <div className="flex flex-col items-start justify-center gap-6 pt-4">
+          <div className="flex flex-col gap-[15px] py-[10px]">
+            <Heading variant="h1">
+              Our platform is free for registered users!
+            </Heading>
+            <p className="text-sm text-muted">
+              So, sign in for free with your Google account for unrestricted
+              access to questions and projects!
+            </p>
+          </div>
           <SignInButton />
-          <p className="font-medium">
-            Support For More Sign In Providers Coming Soon!
+          <p className="text-sm text-muted">
+            We will never share your data with anyone without your permission!
+            Also, you can delete your account anytime you want through settings
+            (once signed in, click on your avatar to open a menu with settings).
           </p>
         </div>
       </>
