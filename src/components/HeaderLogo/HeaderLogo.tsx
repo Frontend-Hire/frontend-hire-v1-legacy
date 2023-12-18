c20bb011b7b12ce4e48ea2bd0464d50979953925
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-12-19 02:23:14 +0530

    fix: logo size

--- src/components/HeaderLogo/HeaderLogo.tsx
+++ src/components/HeaderLogo/HeaderLogo.tsx
@@ -11,7 +11,7 @@ export default function HeaderLogo({ fill = 'WHITE' }: Props) {
     <Image
       src={fill == 'WHITE' ? WhiteLogo : GrayLogo}
       priority={true}
-      className="h-full w-full sm:h-[36px]"
+      className="h-[36px] w-full"
       alt="Frontend Hire"
     />
   );
