b135cd8d79bf5f36af4c3f68786bbaf7591085a8
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-12-21 23:50:27 +0530

    feat: icon

--- src/app/(protected)/interviews/simulated-phone-screening/page.tsx
+++ src/app/(protected)/interviews/simulated-phone-screening/page.tsx
@@ -1,6 +1,4 @@
-import CardLinkItem from '@/components/CardLinkItem';
 import Heading from '@/components/Heading';
-import VisuallyHidden from '@/components/ui/visually-hidden';
 import { PhoneCallIcon } from 'lucide-react';
 
 import { Metadata } from 'next';
@@ -13,11 +11,14 @@ export const metadata: Metadata = {
 export default async function Projects() {
   return (
     <main className="flex flex-col gap-[20px] p-[10px] md:px-[100px] md:py-[20px] lg:px-[200px] xl:px-[250px]">
-      <div className="flex flex-col gap-[15px] py-[10px]">
-        <Heading variant="h1">Simulated Phone Screening</Heading>
-        <p className="text-sm text-muted">
-          One of the first rounds, so first impression should be great
-        </p>
+      <div className="flex justify-between gap-[10px]">
+        <div className="flex flex-col gap-[15px] py-[10px]">
+          <Heading variant="h1">Simulated Phone Screening</Heading>
+          <p className="text-sm text-muted">
+            One of the first rounds, so first impression should be great
+          </p>
+        </div>
+        <PhoneCallIcon size={60} />
       </div>
     </main>
   );
