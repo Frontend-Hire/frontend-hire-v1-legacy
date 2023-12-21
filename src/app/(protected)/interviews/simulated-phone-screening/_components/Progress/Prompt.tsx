7ddbac411b5f9d4c8e20f6b8127faa99441399d6
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-12-22 03:38:37 +0530

    feat: flowing text

--- src/app/(protected)/interviews/simulated-phone-screening/_components/Progress/Prompt.tsx
+++ src/app/(protected)/interviews/simulated-phone-screening/_components/Progress/Prompt.tsx
@@ -5,10 +5,32 @@ import { useSpeechSynthesis } from '../../_context/SpeechSynthesisContext';
 
 interface Props {
   isActive: boolean;
+  text?: string;
 }
 
-export default function Prompt({ isActive }: Props) {
+export default function Prompt({
+  isActive,
+  text = 'Hi, am I speaking to username?',
+}: Props) {
   const { speak, cancel } = useSpeechSynthesis();
+  const [displayText, setDisplayText] = React.useState('');
+  const [charIndex, setCharIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = 50;
+    const timer = setInterval(() => {
+      if (charIndex < text.length) {
+        setDisplayText(text.substring(0, charIndex + 1));
+        setCharIndex(charIndex + 1);
+      } else {
+        clearInterval(timer);
+      }
+    }, interval);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [text, charIndex]);
 
   React.useEffect(() => {
     if (isActive) {
@@ -28,7 +50,7 @@ export default function Prompt({ isActive }: Props) {
     <div className="flex flex-col overflow-hidden rounded bg-card">
       <div className="bg-primary px-[10px] py-[5px] font-medium">RECRUITER</div>
       <div className="flex gap-[5px] p-[10px]">
-        <span className="grow">Hi, am I speaking to username?</span>
+        <span className="grow">{displayText}</span>
         {isActive && (
           <Button
             onClick={onPardon}
