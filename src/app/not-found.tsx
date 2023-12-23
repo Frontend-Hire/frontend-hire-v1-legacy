2cd3d3f6a1818d7945f09d776699ae950d1a01af
Hruthik Reddy Yarala <32637966+yaralahruthik@users.noreply.github.com>
2023-12-23 20:04:16 +0530

    Questions (#63)
    * fix: job application difficulty
    
    * feat: 404 not found
    
    * feat: design-system-buttons-1
    
    * feat: refactor question

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Heading from '@/components/Heading';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <main className="flex flex-col gap-[20px] p-[10px] md:px-[100px] md:py-[20px] lg:px-[200px] xl:px-[250px]">
+        <Heading variant="h1">404: Not Found</Heading>
+        <p className="muted">Oops, how did you even come here?</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
