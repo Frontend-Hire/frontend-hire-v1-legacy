f952cd4f2d19ce4292711e22fb2b81bc9ba40805
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-01-13 20:32:02 +0530

    refactor: projects layout

--- src/app/(protected)/projects/[projectId]/page.tsx
+++ src/app/(protected)/projects/[projectId]/page.tsx
@@ -1,51 +1,5 @@
-'use client';
-
-import PrimaryLayout from './_layout/PrimaryLayout';
-import Header from './_components/Header';
-import ProjectLayoutItem from '@/components/ProjectLayoutItem';
-import useProject from './_hooks/useProject';
-import ResetProgressButtonWithAlert from './_components/ResetProgressButtonWithAlert';
-import HeaderSkeleton from '@/components/HeaderSkeleton';
-import ProjectLayoutSkeleton from '@/components/ProjectLayoutSkeleton';
+import ClientContainer from './_components/ClientContainer';
 
 export default function ProjectPage() {
-  const { data } = useProject();
-
-  if (data.status === 'loading' || data.status === 'idle')
-    return (
-      <PrimaryLayout header={<HeaderSkeleton />}>
-        <ProjectLayoutSkeleton />
-      </PrimaryLayout>
-    );
-
-  if (data.status === 'error') {
-    throw new Error(data.message);
-  }
-
-  if (data.project == undefined) {
-    throw new Error('This was unexepected');
-  }
-
-  return (
-    <PrimaryLayout header={<Header />}>
-      <ProjectLayoutItem
-        rightButtons={
-          <>
-            <ResetProgressButtonWithAlert />
-          </>
-        }
-        tabs={[
-          {
-            label: 'Project',
-            value: 'Project',
-            content: (
-              <div className="prose prose-invert max-w-none bg-[#151515] p-4">
-                {data.project.getContent()}
-              </div>
-            ),
-          },
-        ]}
-      />
-    </PrimaryLayout>
-  );
+  <ClientContainer />;
 }
