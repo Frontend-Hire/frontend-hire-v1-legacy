3eeea62fce5afe7a8111a264334a698f23fa0781
Hruthik Reddy Yarala <32637966+yaralahruthik@users.noreply.github.com>
2024-01-19 02:48:56 +0530

    Upgrade/next (#77)
    * feat: next bump
    
    * feat: client size rendering

--- src/app/questions/_components/QuestionFilters.tsx
+++ src/app/questions/_components/QuestionFilters.tsx
@@ -1,25 +1,13 @@
 'use client';
 
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useQueryState } from 'nuqs';
 import { ListFilterIcon, SearchIcon } from 'lucide-react';
 
 import VisuallyHidden from '../../../components/ui/visually-hidden';
 import InputWithIcon from '@/components/InputWithIcon';
 
 export default function QuestionFilters() {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-
-  const handleSearch = (value: string) => {
-    const params = new URLSearchParams(searchParams);
-    if (value) {
-      params.set('search', value);
-    } else {
-      params.delete('search');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  };
+  const [search, setSearch] = useQueryState('search');
 
   return (
     <div className="flex flex-wrap items-center gap-[10px]">
@@ -34,8 +22,8 @@ export default function QuestionFilters() {
         <InputWithIcon
           icon={<SearchIcon />}
           id="search"
-          defaultValue={searchParams.get('search')?.toString()}
-          onChange={(e) => handleSearch(e.target.value)}
+          defaultValue={search || ''}
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Search Questions..."
         />
       </div>
