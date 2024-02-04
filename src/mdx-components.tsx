c9472926547a197522d9136677e6c9ce86388255
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-04 06:32:00 +0530

    feat: mdx auto convert links

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) => {
+      const isInternalLink =
+        props.href &&
+        (props.href.startsWith('/') || props.href.startsWith('#'));
+      if (isInternalLink) {
+        return (
+          <Link href={props.href!}>
+            <a {...props} />
+          </Link>
+        );
+      }
+
+      return <a target="_blank" {...props} />;
+    },
     ...components,
   };
 }
