04fb4efe1b1e29bdc4debbab29f1bfbf96a6a781
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-07 19:49:00 +0530

    fix: shadcn components

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React from 'react';
+import * as React from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
 import { cn } from '@/lib/utils';
 
 const TooltipProvider = TooltipPrimitive.Provider;
 
-const TooltipRoot = TooltipPrimitive.Root;
+const Tooltip = TooltipPrimitive.Root;
 
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
@@ -19,7 +19,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      'z-[100] max-w-[60ch] overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
       className,
     )}
     {...props}
@@ -27,20 +27,4 @@ const TooltipContent = React.forwardRef<
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
-type TooltipProps = {
-  title: React.ReactNode;
-};
-
-export default function Tooltip({
-  title,
-  children,
-}: React.PropsWithChildren<TooltipProps>) {
-  return (
-    <TooltipProvider delayDuration={0}>
-      <TooltipRoot>
-        <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent className="z-[200]">{title}</TooltipContent>
-      </TooltipRoot>
-    </TooltipProvider>
-  );
-}
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
