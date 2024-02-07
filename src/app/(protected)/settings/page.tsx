5b4f6d70cdc32d2b3730be4e9407a22a4dd503ff
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-07 20:25:33 +0530

    fix: settings page

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,13 +3,16 @@ import DeleteAccountButtonWithAlert from './_components/DeleteAccountButtonWithA
 import DeleteCodeSubmissionsButtonWithAlert from './_components/DeleteCodeSubmissionsButtonWithAlert';
 import DeleteProjectSubmissionsButtonWithAlert from './_components/DeleteProjectSubmissionsButtonWithAlert';
 import UserInfo from './_components/UserInfo';
+import CustomHeading from '@/components/CustomHeading';
 
 export default function Settings() {
   return (
     <main className="flex flex-col gap-[20px] p-[10px] md:px-[100px] md:py-[20px] lg:px-[200px] xl:px-[250px]">
-      <div className="flex flex-col gap-[15px] py-[10px] text-center">
-        <Heading variant="h1">Settings</Heading>
-      </div>
+      <CustomHeading
+        title="Settings"
+        subTitle="Manage all your account related stuff"
+      />
+
       <UserInfo />
       <div className="mt-[40px] flex flex-col items-center justify-center gap-[30px]">
         <DeleteCodeSubmissionsButtonWithAlert />
