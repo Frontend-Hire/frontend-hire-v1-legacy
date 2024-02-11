4a039a3c64c22e9e7e2caea990af9e39017253e8
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-11 17:39:07 +0530

    fix: clean up

--- src/app/(landing page)/page.tsx
+++ src/app/(landing page)/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-[10px] py-2">
       <HeroSection />
-      <section className="bg-red-500">Hello World!</section>
     </main>
   );
 }
