4184d8d8464a1c7277327944c06fad6924610a46
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-13 18:59:41 +0530

    feat: disable sentry for dev

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: 'https://a0e001bca1ee07517271fc72de98376c@o4506388139278336.ingest.sentry.io/4506388141441024',
-
+  enabled: process.env.NODE_ENV === 'production',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
