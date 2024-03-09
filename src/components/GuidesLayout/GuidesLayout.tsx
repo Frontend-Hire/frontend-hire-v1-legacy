0ec8ac5737003d71b463af5f0a249222abb56a44
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-03-09 21:07:23 +0530

    fix: max width

--- src/components/GuidesLayout/GuidesLayout.tsx
+++ src/components/GuidesLayout/GuidesLayout.tsx
@@ -1,7 +1,7 @@
 export default function GuidesLayout({ children }: React.PropsWithChildren) {
   return (
     <article className="flex flex-col gap-[20px]">
-      <div className="prose prose-invert max-w-none py-[10px] prose-h2:mt-5">
+      <div className="prose prose-invert mx-auto py-[10px] prose-h2:mt-5">
         {children}
       </div>
     </article>
