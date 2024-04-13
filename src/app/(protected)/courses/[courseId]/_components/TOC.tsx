fb340889c589d09f93c43b14b2572ed87534aa91
Ashish Khare <akstyles211@gmail.com>
2024-04-13 19:28:28 +0530

    fix: add intersection observer for toc and steps component fixes

--- src/app/(protected)/courses/[courseId]/_components/TOC.tsx
+++ src/app/(protected)/courses/[courseId]/_components/TOC.tsx
@@ -10,7 +10,48 @@ type TOCProps = {
 };
 
 export default function TOC({ headings }: TOCProps) {
-  const [currentHeading, setCurrentHeading] = React.useState(headings[0].title);
+  const [currentHeading, setCurrentHeading] = React.useState<string>(
+    headings[0].id,
+  );
+
+  const getSections = React.useCallback(() => {
+    // This should be HTML/DOM element
+    let sections: any = [];
+
+    headings.forEach(({ id }) => {
+      const element = document.getElementById(id);
+
+      // add element to the sections stack
+      if (Boolean(element)) {
+        sections.push(element);
+      }
+    });
+
+    return sections;
+  }, [headings]);
+
+  // listener for intersection observer
+  React.useEffect(() => {
+    const handleScroll = () => {
+      const sections = getSections();
+      let closestSectionId = null;
+      let closestSectionTop = Infinity;
+
+      sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.top < closestSectionTop) {
+          closestSectionId = section.id;
+          closestSectionTop = rect.top;
+        }
+      });
+
+      setCurrentHeading(closestSectionId);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [getSections]);
 
   return (
     <aside className="sticky top-20 hidden max-h-screen w-[250px] lg:block">
