b8201eb030b2ea9803269a8bce3143166372daf9
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-13 16:35:53 +0530

    fix: responsive position

--- src/app/(protected)/courses/[courseId]/_components/TOC.tsx
+++ src/app/(protected)/courses/[courseId]/_components/TOC.tsx
@@ -13,33 +13,31 @@ export default function TOC({ headings }: TOCProps) {
   const [currentHeading, setCurrentHeading] = React.useState(headings[0].title);
 
   return (
-    <aside className="hidden max-h-screen w-[250px] lg:block">
-      <div className="fixed">
-        <p className="mb-2 text-sm font-medium">On This Page</p>
-        <nav>
-          <ul className="space-y-1">
-            {headings.map((item, index) => (
-              <li
-                key={index}
-                className={cn(
-                  'text-sm text-muted transition-colors',
-                  item.id === currentHeading && 'text-ring',
-                  item.level === 2 && 'ml-2',
-                  item.level === 3 && 'ml-4',
-                )}
+    <aside className="sticky top-20 hidden max-h-screen w-[250px] lg:block">
+      <p className="mb-2 text-sm font-medium">On This Page</p>
+      <nav>
+        <ul className="space-y-1">
+          {headings.map((item, index) => (
+            <li
+              key={index}
+              className={cn(
+                'text-sm text-muted transition-colors',
+                item.id === currentHeading && 'text-ring',
+                item.level === 2 && 'ml-2',
+                item.level === 3 && 'ml-4',
+              )}
+            >
+              <Link
+                href={`#${item.id}`}
+                className="hover:underline"
+                onClick={() => setCurrentHeading(item.id)}
               >
-                <Link
-                  href={`#${item.id}`}
-                  className="hover:underline"
-                  onClick={() => setCurrentHeading(item.id)}
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 }
