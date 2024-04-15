3df2acdd96803d8648fffd3635275d52faa74d09
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-15 15:33:15 +0530

    fix: code padding

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -10,7 +10,7 @@ export default function Code({
   return (
     <code
       className={cn(
-        'px-4 py-2 text-sm',
+        'p-4 text-sm',
         hasLineNumbers && '[counter-reset:line]',
         className,
       )}
