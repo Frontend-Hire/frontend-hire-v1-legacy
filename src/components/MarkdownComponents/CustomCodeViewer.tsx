b7f1d535c4473223a8a131ddbc84f1692d85321d
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-19 01:30:08 +0530

    feat: stackpack checkpoint

--- src/components/MarkdownComponents/CustomCodeViewer.tsx
+++ src/components/MarkdownComponents/CustomCodeViewer.tsx
@@ -11,7 +11,7 @@ export default function CustomCodeViewer({
   filename,
 }: React.PropsWithChildren<CustomCodeViewerProps>) {
   return (
-    <div className="not-prose relative overflow-hidden rounded">
+    <div className="not-prose relative my-2 overflow-hidden rounded">
       {filename && (
         <p className="bg-primary px-2 py-1 font-medium">{filename}</p>
       )}
