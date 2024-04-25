60b7785d5636dc3ab184472b3a3d062df038d26a
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-25 13:19:28 +0530

    feat: seo

--- src/app/(landing page)/_components/QuestionsFeature/QuestionsFeature.tsx
+++ src/app/(landing page)/_components/QuestionsFeature/QuestionsFeature.tsx
@@ -1,9 +1,15 @@
 import { getQuestionsFromLocal } from '@/lib/fetchLocalFiles';
 
 import QuestionsCarousel from './QuestionsCarousel';
+import VisuallyHidden from '@/components/ui/visually-hidden';
 
 export default async function QuestionsFeature() {
   const questions = await getQuestionsFromLocal();
 
-  return <QuestionsCarousel questions={questions} />;
+  return (
+    <>
+      <VisuallyHidden>List of Questions</VisuallyHidden>
+      <QuestionsCarousel questions={questions} />
+    </>
+  );
 }
