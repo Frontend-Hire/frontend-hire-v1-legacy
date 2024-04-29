5265601db74642a5c3b26235dae8f639eb73e4ba
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-29 19:38:06 +0530

    feat: react cohort page

--- src/app/(regular-layout)/react-cohort/layout.tsx
+++ src/app/(regular-layout)/react-cohort/layout.tsx
@@ -13,11 +13,11 @@ export default function ReactCohort({ children }: React.PropsWithChildren) {
         />
       </div>
 
-      <div className="prose prose-invert mx-auto px-4 py-2 marker:text-[#FF5CF2] prose-h2:mt-5 prose-strong:text-[#FF5CF2] sm:px-6 lg:px-8 lg:pe-8">
+      <div className="container prose prose-invert py-2 marker:text-[#FF5CF2] prose-h2:mt-5 prose-strong:text-[#FF5CF2]">
         {children}
       </div>
 
-      <div className="mb-8 rounded-xl border-2 bg-zinc-950 px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+      <div className="mb-8 w-full rounded-xl border-2 bg-card px-4 py-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
             Learn Core React and some of its ecosystem in 7 days.
@@ -27,7 +27,12 @@ export default function ReactCohort({ children }: React.PropsWithChildren) {
             most of the time in your projects. We will also cover some of the
             most popular libraries in the React Ecosystem.
           </p>
-          <Button className="rounded-2 mt-8 p-2 px-4">Save your spot</Button>
+          <Button
+            size="lg"
+            className="rounded-2 mt-8 w-full py-6 text-lg font-bold capitalize sm:py-8 sm:text-2xl"
+          >
+            Save your spot
+          </Button>
         </div>
       </div>
     </article>
