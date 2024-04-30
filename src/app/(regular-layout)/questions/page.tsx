a90e35437f268b50ae748b12392a9cc3f2a7c66e
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-04-30 20:21:52 +0530

    feat: questions layout

--- src/app/(regular-layout)/questions/page.tsx
+++ src/app/(regular-layout)/questions/page.tsx
@@ -2,9 +2,7 @@ import { Metadata } from 'next';
 import Container from './_components/Container';
 import { Suspense } from 'react';
 import QuestionItemSkeleton from '@/components/QuestionItemSkeleton';
-import CustomHeading from '@/components/CustomHeading';
 import { openGraphShared } from '@/app/shared-metadata';
-import QuestionFilters from './_components/QuestionFilters';
 
 export const metadata: Metadata = {
   title: 'Questions | Frontend Hire',
@@ -19,24 +17,16 @@ export const metadata: Metadata = {
 
 export default function Questions() {
   return (
-    <article className="flex flex-col gap-5">
-      <CustomHeading
-        title="Questions"
-        subTitle="Meant for real world and interview based scenarios."
-      />
-
-      <QuestionFilters />
-      <Suspense
-        fallback={
-          <div className="flex flex-col gap-5">
-            {Array.from({ length: 20 }).map((_, index) => (
-              <QuestionItemSkeleton key={index} />
-            ))}
-          </div>
-        }
-      >
-        <Container />
-      </Suspense>
-    </article>
+    <Suspense
+      fallback={
+        <div className="flex flex-col gap-5">
+          {Array.from({ length: 20 }).map((_, index) => (
+            <QuestionItemSkeleton key={index} />
+          ))}
+        </div>
+      }
+    >
+      <Container />
+    </Suspense>
   );
 }
