15f21b5f783a03b99043883838c624c7af31c54b
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-05-22 23:59:15 +0530

    feat: pro updates

--- src/components/PremiumProtectedContentLayout/PremiumProtectedContentLayout.tsx
+++ src/components/PremiumProtectedContentLayout/PremiumProtectedContentLayout.tsx
@@ -18,12 +18,10 @@ export default async function PremiumProtectedContentLayout({
   }
 
   return (
-    <>
-      <div className="flex flex-col gap-4">
-        <Heading variant="h1">Get access to pro content</Heading>
-        <p>This content is only available to pro users. Get Pro Now.</p>
-        <Button>Get Pro</Button>
-      </div>
-    </>
+    <div className="flex w-full flex-col items-center gap-4 py-10">
+      <Heading variant="h1">Get access to Pro content</Heading>
+      <p>This content is exclusive to pro users. Get Pro Now.</p>
+      <Button>Get Pro</Button>
+    </div>
   );
 }
