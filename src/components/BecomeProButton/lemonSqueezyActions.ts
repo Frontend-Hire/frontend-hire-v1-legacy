14e60af5120aeb2299dbc78c3562a064be1cd6a2
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-06-17 02:35:20 +0530

    fix: clean up

--- src/components/BecomeProButton/lemonSqueezyActions.ts
+++ src/components/BecomeProButton/lemonSqueezyActions.ts
@@ -1,32 +1,9 @@
 'use server';
 
 import { giveProAccess } from './proAction';
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/supabase';
-
-const supabaseAdminClient = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-    },
-  },
-);
 
 export const processProPlan = async (data: any) => {
-  const orderId = data.id;
-  const currency = data.currency;
-  const total = data.total;
   const userId = data.meta.custom_data.user_id;
-
-  await supabaseAdminClient.from('lemonsqueezy_orders').insert({
-    order_id: orderId,
-    currency,
-    amount: total,
-    user_id: userId,
-  });
 
   await giveProAccess(userId);
 };
