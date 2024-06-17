04d6eb4a235434818806ef685e9caa0fb1322efa
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-06-18 00:48:42 +0530

    feat: improvements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,26 @@
-import AvatarDropdown from './AvatarDropdown';
-import SignInButton from '../SignInButton';
 import Link from 'next/link';
 import HeaderLogo from '../HeaderLogo';
-import createSupabaseServerClient from '@/lib/supabase/supabaseServerClient';
 import CustomNavigationMenu from '../CustomNavigationMenu';
 import MobileMenu from './MobileMenu';
+import React from 'react';
+import AvatarContainer from './AvatarContainer';
 
-export default async function Header() {
-  const supabaseServerClient = createSupabaseServerClient();
-
-  const {
-    data: { user },
-  } = await supabaseServerClient.auth.getUser();
-
-  const getLogoLink = () => {
-    return '/';
-  };
-
+export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b border-border/40 bg-background/90 backdrop-blur-xl">
       <div className="container flex h-14 w-full items-center justify-between gap-4 p-2 md:py-2">
-        <Link prefetch={false} href={getLogoLink()}>
+        <Link prefetch={false} href="/">
           <HeaderLogo />
         </Link>
-
-        <MobileMenu user={user} />
+        <React.Suspense>
+          <MobileMenu />
+        </React.Suspense>
 
         <div className="hidden items-center gap-4 text-sm font-medium sm:flex md:gap-[30px] md:text-base">
           <CustomNavigationMenu />
-          {user ? (
-            <AvatarDropdown
-              picture={user.user_metadata.picture}
-              name={user.user_metadata.name}
-            />
-          ) : (
-            <SignInButton />
-          )}
+          <React.Suspense>
+            <AvatarContainer />
+          </React.Suspense>
         </div>
       </div>
     </header>
