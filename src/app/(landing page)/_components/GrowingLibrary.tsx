991497975117463ea0a66edbd135374310517c2e
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-06-20 17:52:22 +0530

    feat: home page free references

--- src/app/(landing page)/_components/GrowingLibrary.tsx
+++ src/app/(landing page)/_components/GrowingLibrary.tsx
@@ -4,9 +4,8 @@ export default function GrowingLibrary() {
   return (
     <div className="flex w-full flex-col gap-4">
       <p className="text-sm text-muted md:text-base">
-        A Growing Library of <strong>30+ Questions</strong>,{' '}
-        <strong>10+ Projects</strong>, <strong>2+ System Designs</strong>, and
-        <strong> 2+ Courses</strong>
+        A Growing Library of <strong>20+ Questions</strong> and
+        <strong> 4+ Courses</strong>!
       </p>
       <CustomCarousel />
     </div>
