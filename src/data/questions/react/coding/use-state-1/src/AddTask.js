9273e352d910500d47dd3bf6323f20ddd08b9c74
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-07-16 18:25:23 +0530

    fix: add task question component

--- src/data/questions/react/coding/use-state-1/src/AddTask.js
+++ src/data/questions/react/coding/use-state-1/src/AddTask.js
@@ -3,13 +3,14 @@ export const code = `import React from 'react';
 export default function AddTask({ onAdd }) {
   const [task, setTask] = React.useState('');
 
-  const handleAddTask = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     onAdd(task);
     setTask('');
   };
 
   return (
-    <div className="mb-4">
+    <form onSubmit={handleSubmit} className="mb-4">
       <input
         placeholder="Add Task"
         className="border p-2"
@@ -17,12 +18,12 @@ export default function AddTask({ onAdd }) {
         onChange={(e) => setTask(e.target.value)}
       />
       <button
-        onClick={handleAddTask}
+        type='submit'
         className="border border-green-500 bg-green-500 p-2 text-white"
       >
         Add
       </button>
-    </div>
+    </form>
   );
 }
 `;
