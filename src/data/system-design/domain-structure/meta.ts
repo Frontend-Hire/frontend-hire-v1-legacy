a7c6c1243d0fad895f58310cbf0dd93ba2e3d725
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-07 14:01:48 +0530

    feat: checkpoint

---
+++ src/data/system-design/domain-structure/meta.ts
@@ -0,0 +1,16 @@
+import { SystemDesign } from '@/types/SystemDesign';
+
+export const meta: SystemDesign = {
+  id: 'domain-structure',
+  title: 'Design the domain structure for a web application',
+  description:
+    'How would you setup the domains for company with multiple user facing web apps?',
+  link: '/system-design/domain-structure/overview',
+  isPro: true,
+  isPublished: true,
+  publishedOn: new Date('2024-08-08'),
+  chapters: {
+    overview: 'Overview',
+    summary: 'Summary',
+  },
+};
