445f03335a94b1bef81d79a23c4755999109cc18
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-12 19:50:56 +0530

    feat: polyfill first question

---
+++ src/data/questions/javascript/coding/polyfill-array-at/meta.ts
@@ -0,0 +1,18 @@
+import {
+  CODING_ENVIRONMENT_TYPE,
+  DIFFICULTY,
+  Question,
+  QUESTION_TYPE,
+} from '@/types/Question';
+
+export const meta: Question = {
+  id: 'polyfill-array-at',
+  title: 'Polyfill: Array.at() Method',
+  description:
+    'The Array.at() method takes an integer value and returns the item at that index, allowing for positive and negative integers. Negative integers count back from the last item in the array.',
+  type: QUESTION_TYPE.CODING,
+  environment: CODING_ENVIRONMENT_TYPE.LOCAL,
+  publishedOn: new Date('2024-08-13'),
+  difficulty: DIFFICULTY.EASY,
+  isFree: true,
+};
