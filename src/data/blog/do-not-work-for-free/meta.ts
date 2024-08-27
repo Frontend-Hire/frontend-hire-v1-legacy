5ac93b5e19d62f43d128f93dcd663762cdc19646
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-27 16:16:00 +0530

    feat: blog checkpoint

---
+++ src/data/blog/do-not-work-for-free/meta.ts
@@ -0,0 +1,22 @@
+import authorImg from '@/assets/team/hruthikReddyInterviewer.jpeg';
+import coverImg from './cover.webp';
+import { BlogMeta } from '@/types/Blogs';
+
+export const meta: BlogMeta = {
+  title: 'Do not work for free',
+  description: 'Instead pay up for some mentorship!',
+  authors: [
+    {
+      name: 'Hruthik Reddy',
+      image: authorImg,
+      linkedIn: 'https://www.linkedin.com/in/yarala-hruthik-reddy/',
+      twitter: 'https://twitter.com/thisisyhr',
+      website: 'https://www.iamyhr.com/',
+      github: 'https://github.com/yaralahruthik',
+    },
+  ],
+  cover: coverImg,
+  publishedOn: '2024-08-27',
+  lastUpdated: '2024-08-27',
+  isPublished: true,
+};
