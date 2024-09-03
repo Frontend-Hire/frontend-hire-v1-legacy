7fbaa8022e1ca1d34cc3bff91ff22d04026c7d1b
Hruthik Reddy Yarala <32637966+yaralahruthik@users.noreply.github.com>
2024-09-03 11:07:35 +0530

    TypeScript: Migrate useVolume (#163)
    * feat: question checkpoint
    
    * feat: question ts
    
    * feat: resources
    
    * feat: solution semantic

---
+++ src/data/questions/react/coding/ts-migrate-use-volume/meta.ts
@@ -0,0 +1,17 @@
+import {
+  CODING_ENVIRONMENT_TYPE,
+  DIFFICULTY,
+  Question,
+  QUESTION_TYPE,
+} from '@/types/Question';
+
+export const meta: Question = {
+  id: 'ts-migrate-use-volume',
+  title: 'TypeScript: Migrate useVolume',
+  description:
+    'Remember our question "Hooks: useVolume" but that was in JavaScript. Try to migrate it to TypeScript.',
+  type: QUESTION_TYPE.CODING,
+  environment: CODING_ENVIRONMENT_TYPE.LOCAL,
+  publishedOn: new Date('2024-09-02'),
+  difficulty: DIFFICULTY.HARD,
+};
