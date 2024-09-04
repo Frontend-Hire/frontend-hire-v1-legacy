58178bb4ff4aac3aeec7919c326c2ce432524fd6
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-04 22:15:18 +0530

    refactor: question id

--- src/data/questions/react/coding/use-mouse/meta.ts
+++ src/data/questions/react/coding/use-mouse/meta.ts
@@ -6,8 +6,8 @@ import {
 } from '@/types/Question';
 
 export const meta: Question = {
-  id: 'use-mouse-I',
-  title: 'Hooks: useMouse - I',
+  id: 'use-mouse',
+  title: 'Hooks: useMouse',
   description:
     'Create a custom hook that tracks the mouse position. The hook should return the x and y coordinates of the mouse and update them on mouse move, it should also give mouse position relative to a element.',
   type: QUESTION_TYPE.CODING,
