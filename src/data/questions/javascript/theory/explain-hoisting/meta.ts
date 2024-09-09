b40b974ae24a4abbef244e9a240d735bbb19a692
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-09 12:24:18 +0530

    feat: hoisting

---
+++ src/data/questions/javascript/theory/explain-hoisting/meta.ts
@@ -0,0 +1,12 @@
+import { DIFFICULTY, QUESTION_TYPE, Question } from '@/types/Question';
+
+export const meta: Question = {
+  id: 'explain-hoisting',
+  title: 'Explain Hoisting',
+  description:
+    'You do not have to worry much about Hoisting in the real world but for interviews this is a must to know topic!',
+  type: QUESTION_TYPE.THEORY,
+  difficulty: DIFFICULTY.EASY,
+  publishedOn: new Date('2024-09-09'),
+  isFree: true,
+};
