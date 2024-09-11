c9251e6f57c9e79f862d6723b5693ee4eb589bca
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-11 18:36:42 +0530

    feat: question requirements

--- src/data/questions/javascript/coding/vanilla-tic-tac-toe/src/index.html.js
+++ src/data/questions/javascript/coding/vanilla-tic-tac-toe/src/index.html.js
@@ -1,17 +1,13 @@
-export const code = `<!DOCTYPE html>
+export const code = `<!doctype html>
 <html>
+  <head>
+    <title>FH: Tic-Tac-Toe</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="/styles.css" />
+  </head>
 
-<head>
-  <title>FH: Tic-Tac-Toe</title>
-  <meta charset="UTF-8" />
-  <link rel="stylesheet" href="/styles.css" />
-</head>
-
-<body>
-  <div id="app"></div>
-
-  <script src="index.js">
-  </script>
-</body>
-
-</html>`;
+  <body>
+    <script src="index.js"></script>
+  </body>
+</html>
+`;
--- src/data/questions/javascript/coding/vanilla-tic-tac-toe/src/index.js
+++ src/data/questions/javascript/coding/vanilla-tic-tac-toe/src/index.js
@@ -1,1 +1,1 @@
-export const code = `document.getElementById("app").innerHTML = \`<h1>Hello world</h1>\`;`;
+export const code = ``;
