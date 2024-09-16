892b7749178187d2dafdf1ee0b670f84434f8626
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-16 13:17:38 +0530

    refactor: image formats and question id

--- src/data/questions/dsa/theory/what-is-a-linked-list/meta.ts
+++ src/data/questions/dsa/theory/what-is-a-linked-list/meta.ts
@@ -1,7 +1,7 @@
 import { DIFFICULTY, QUESTION_TYPE, Question } from '@/types/Question';
 
 export const meta: Question = {
-  id: 'linked-list',
+  id: 'what-is-a-linked-list',
   title: 'What is a Linked List?',
   description:
     'Linked List is a linear data structure. It is a collection of nodes. Each node contains a value and a pointer to the next node in the list.',
