be0e27168b0cbe0e732f9e357c1957e62d058175
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-18 13:16:04 +0530

    fix: dsa coding removed

--- src/config/questionCategories.ts
+++ src/config/questionCategories.ts
@@ -13,11 +13,6 @@ export const QUESTION_CATEGORIES: QuestionCategory[] = [
     className: `hover:bg-white hover:text-black`,
   },
   {
-    id: 'dsa/coding',
-    title: 'DSA Coding',
-    className: `hover:bg-white hover:text-black`,
-  },
-  {
     id: 'web/theory',
     title: 'Web Theory',
     className: `hover:bg-primary`,
