e32812ab3a416e09cb27e800060bded9b45bb212
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-09-19 00:07:13 +0530

    fix: heading skill

--- src/utils/skill.ts
+++ src/utils/skill.ts
@@ -2,8 +2,11 @@ import { capitalize } from './text';
 
 const SKILL_MAP: { [key: string]: string } = {
   next: 'Next.js',
+  css: 'CSS',
+  dsa: 'DSA',
+  javascript: 'JavaScript',
 };
 
 export const getSkill = (skill: string) => {
-  return SKILL_MAP[skill] || capitalize(skill);
+  return SKILL_MAP[skill.toLowerCase()] || capitalize(skill.toLowerCase());
 };
